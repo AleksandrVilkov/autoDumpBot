651ef49bd9998e5837ff91ef96ae9e375ed1a124
vilkov <alexander.vilkov@gridnine.com>
2023-03-30 17:54:33 +0300

    firstCommit

--- internal/usecase.go
+++ internal/usecase.go
@@ -1,41 +1,5 @@
 package internal
 
-func CreateWelcomeMsg() string {
-	return getResources().Msgs.WelcomeMessage
-}
-
-func CreateRulesMsg() string {
-	return getResources().Msgs.Rules
-}
-
-func CreateErrAuthMsg(chUrl string) string {
-	return getResources().Errors.AuthError + " \nПодпишись: \n" + chUrl
-}
-
-func CreatePreRegistrationMsg() string {
-	return getResources().Msgs.PreRegistration
-}
-
-func CreateSuccessCarBrandEnter() string {
-	return getResources().Msgs.SuccessCarBrandEnter
-}
-func CreateSuccessCarModelEnter() string {
-	return getResources().Msgs.SuccessCarModelEnter
-}
-func CreateSuccessCarEngineEnter() string {
-	return getResources().Msgs.SuccessCarEngineEnter
-}
-func CreateError() string {
-	return getResources().Errors.CommonError
-}
-
 func CreatesReservation() string {
 	return getResources().Msgs.SuccessReservation
-}
-
-func CreateSubscriptionSaleMsg() string {
-	return "CreateSubscriptionSaleMsg"
-}
-func CreateStartSaleMsg() string {
-	return "CreateStartSaleMsg"
 }
